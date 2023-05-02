e9e06862db26640f3d7ed9ac8df4f9cc8b7c0a19
Tomislav Kolobaric <kolobaric@hotmail.com>
2023-05-02 17:04:46 +0200

    Fixed some issues with navbar and heroImg

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Hero() {
-  return <div className="hero"> </div>;
+  return (
+    <div className="hero">
+      <img
+        src="https://en.louisvuitton.com/images/is/image/PUSH3_IconicHandbags_NewWave_DI3.jpg?wid=2048"
+        alt="heroImg"
+      />
+    </div>
+  );
 }
 
 export default Hero;
